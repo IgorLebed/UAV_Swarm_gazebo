ea56ad4e9c8f501737315dc55fa45aa85e38f2c8
Konstantin Kamynin <konstvictorovich@mail.ru>
2021-08-20 11:54:20 +0300

    Fixed middle point evaluation

--- catkin_ws/src/targets_mid_point/mid_point_talker.py
+++ catkin_ws/src/targets_mid_point/mid_point_talker.py
@@ -3,25 +3,22 @@ import rospy
 from geometry_msgs.msg import Point
 from gazebo_msgs.msg import ModelStates
 from std_msgs.msg import Float64MultiArray
+from uavis.msg import TargetCoordinates
 import utils
 
 def gazebo_co_callback(data):
 	targets_positions = []
-	names = data.name
-	positions = data.pose
-	twists = data.twist
-	new_names = {k: v for v, k in enumerate(names)}
-	heightmap_index = new_names["heightmap"]
-	for i in range(len(names)): 
-		if i != heightmap_index and names[i][:4] == "p3at" and len(names[i]) <= 10:
-			pos = (positions[i].position.x, positions[i].position.y)
-			targets_positions.append(pos)
+	coordinates = data.coordinates
+	for i in range(len(coordinates)):
+		pos = (coordinates[i].x, coordinates[i].y)
+		targets_positions.append(pos)
 	targets_mid_point = utils.calc_targets_mid_point(targets_positions)
 	middle_point_publisher.publish(targets_mid_point)
+
 
 if __name__ == "__main__":
 	rospy.init_node("targets_mid_point_talker")
 	middle_point_publisher = rospy.Publisher("targets_mid_point", Point, queue_size=10)
-	rospy.Subscriber("/gazebo/model_states", ModelStates, gazebo_co_callback)
+	rospy.Subscriber("/scout0/target_coordinates", TargetCoordinates, gazebo_co_callback)
 	rospy.spin()
 
